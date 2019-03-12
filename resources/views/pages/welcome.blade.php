d227219e3b30997cf390465de4e553a7204a5ef1
rumy14 <rumy103040@gmail.com>
2019-03-12 21:02:36 +0600

    home page middle section

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,65 +3,86 @@
 @section('title', '| Homepage')
 
 @section('content')
-    <div class="row">
+    <div class="row limit">
         <div class="cont">
             <img src="https://www.ittiam.com/wp-content/uploads/2018/02/thinkode-blog-banner-image.jpg" alt="Norway" style="width:100%;">
             <div class="centered">
-                <img src="{{{ asset('images/rumi.jpg') }}}" class="img-circle" alt="Cinque Terre">
+                <img src="{{{ asset('images/rumi.jpg') }}}" class="img-circle" alt="Cinque Terre"><br>
+                <a class="ct-btn-scroll ct-js-btn-scroll" href="#scrolly"><img alt="Arrow Down Icon" src="https://www.solodev.com/assets/anchor/arrow-down.png"></a>
             </div>
             <div class="text-block">
-                <h4>Md Abdullah Al Rumy</h4>
-                <p>Software Engineering student from Dhaka.</p>
+                <h4>Software Testing Tools</h4>
+                <p>Software Engineering Firm in USA/JAPAN/Eupore.</p>
             </div>
         </div>
     </div><br>
-    <div class="row">
+
+    <div class="row" id="scrolly">
         <div class="col-md-1">
         </div>
         <div class="col-md-2">
             <div class="row">
                 <div class="list-group">
                     <div class="list-group-item active">
-                        <h4 class="list-group-item-heading">List group item heading</h4>
-                        <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                            to leave the site</p>
+                        <h4 class="list-group-item-heading">Vision</h4>
+                        <p class="list-group-item-text">To earn Global admiration as an IT Outsourcer by delivering eminent Software Services.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <ul class="media-list">
-                    <li class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="{{{ asset('images/pan.jpg') }}}" alt="...">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Media heading</h4>
-                            <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                                to leave the site</p>
-                        </div>
-                    </li>
-                </ul>
+                <div class="list-group">
+                    <div class="list-group-item active">
+                        <h4 class="list-group-item-heading">Mission</h4>
+                        <p class="list-group-item-text">To achieve our goal by providing distinguished software services in the global markets
+                            by constant Innovation in Technology, Illuminating the surroundings by sharing our knowledge and adopting an Integral
+                            approach towards work thus creating a congenial and a nurturing environment that will enable us to increase our revenues
+                            consequently increasing the assets for all our stakeholders.
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <ul>
+                                    <li>Partnering with various clients help in bringing fresh ideas into the firm ensuring our youth in innovation,
+                                        thus improving our quality standards accompanied with growth in the business productivity.</li>
+                                    <li>Being a trustworthy and fair business partner.</li>
+                                    <li>Maintaining and upgrading the quality of work and motivating people to constantly deliver work to client's satisfaction.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="row">
-                <ul class="media-list">
-                    <li class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="{{{ asset('images/pan.jpg') }}}" alt="...">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Media heading</h4>
-                            <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                                to leave the site</p>
-                        </div>
-                    </li>
-                </ul>
+                <div class="list-group">
+                    <div class="list-group-item active">
+                        <h4 class="list-group-item-heading">Values</h4>
+                        <p class="list-group-item-text">We are committed to deliver best industry practices coupled with emerging practical
+                            and dependable technological solutions. Our team is specialized in every aspect of software development.
+                        </p>
+                        <p class="list-group-item-text">We cherish and nurture our values and interactions with all our employees and clients.
+                            We have high regards for individual value which creates a transparent atmosphere ensuring outstanding teamwork and rapport,
+                            built within the company. We promise to live up to our values.
+                        </p>
+                        <b>Our values reflect in:</b>
+                        <ul class="list-unstyled">
+                            <li>
+                                <ul>
+                                    <li>Developing lasting beneficial relationships with our clients</li>
+                                    <li>Confidentiality with respect to work</li>
+                                    <li>Learning and Growing atmosphere</li>
+                                    <li>Integrity expressed</li>
+                                    <li>Independent and Reliable business objectives</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
         </div>
         <div class="col-md-6">
             <div class="panel panel-warning">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <h1 class="panel-title">Web/Mobile Apps Design & Development</h1>
                 </div>
                 <div class="panel-body">
                     <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
@@ -76,7 +97,7 @@
             </div>
             <div class="panel panel-danger">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <h3 class="panel-title">Freelance Consultant</h3>
                 </div>
                 <div class="panel-body">
                     <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
@@ -91,7 +112,37 @@
             </div>
             <div class="panel panel-success">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <h3 class="panel-title">Entrepreneurs</h3>
+                </div>
+                <div class="panel-body">
+                    <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
+                        to leave the site and search elsewhere. If that sounds daunting to you, don't panic. Creating a good blog homepage that makes readers stick around is
+                        all about making the right choices in what content you choose to display, and I’ve got some advice to help you do just that. The purpose of a blog homepage.
+                    </p>
+                    <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
+                        to leave the site and search elsewhere. If that sounds daunting to you, don't panic. Creating a good blog homepage that makes readers stick around is
+                        all about making the right choices in what content you choose to display, and I’ve got some advice to help you do just that. The purpose of a blog homepage.
+                    </p>
+                </div>
+            </div>
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Entrepreneurs</h3>
+                </div>
+                <div class="panel-body">
+                    <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
+                        to leave the site and search elsewhere. If that sounds daunting to you, don't panic. Creating a good blog homepage that makes readers stick around is
+                        all about making the right choices in what content you choose to display, and I’ve got some advice to help you do just that. The purpose of a blog homepage.
+                    </p>
+                    <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
+                        to leave the site and search elsewhere. If that sounds daunting to you, don't panic. Creating a good blog homepage that makes readers stick around is
+                        all about making the right choices in what content you choose to display, and I’ve got some advice to help you do just that. The purpose of a blog homepage.
+                    </p>
+                </div>
+            </div>
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Entrepreneurs</h3>
                 </div>
                 <div class="panel-body">
                     <p class="mb-1">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
@@ -108,41 +159,88 @@
 
         <div class="col-md-2">
             <div class="row">
-                <div class="list-group">
-                    <div class="list-group-item active">
-                        <h4 class="list-group-item-heading">List group item heading</h4>
-                        <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                            to leave the site</p>
-                    </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        On Going Project
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        No. of Products
+                        <span class="badge badge-primary badge-pill">21</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Customers
+                        <span class="badge badge-primary badge-pill">100</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="card mb-3">
+                    <h4 class="card-header">ON/OFF Shore</h4>
+                    <img style="height: 200px; width: 100%; display: block;" src="https://www.tatvasoft.com/images/serviceprovider1.jpg" alt="Card image">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
                 </div>
             </div>
             <div class="row">
-                <ul class="media-list">
-                    <li class="media">
-                        <img class="media-object pull-left" src="{{{ asset('images/pan.jpg') }}}" alt="...">
-                        <div class="media-body">
-                            <h4 class="media-heading">Media heading</h4>
-                            <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                                to leave the site</p>
-                        </div>
+                <div class="card mb-3">
+                    <h4 class="card-header">Developement Process</h4>
+                    <img style="height: 200px; width: 100%; display: block;" src="https://www.tatvasoft.com/images/serviceprovider2.jpg" alt="Card image">
+                </div>
+
+            </div>
+            <div class="row">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        On Going Project
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        No. of Products
+                        <span class="badge badge-primary badge-pill">21</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Customers
+                        <span class="badge badge-primary badge-pill">100</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        On Going Project
+                        <span class="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        No. of Products
+                        <span class="badge badge-primary badge-pill">21</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Customers
+                        <span class="badge badge-primary badge-pill">100</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        No. of Products
+                        <span class="badge badge-primary badge-pill">21</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Customers
+                        <span class="badge badge-primary badge-pill">100</span>
                     </li>
                 </ul>
             </div>
             <div class="row">
-                <ul class="media-list">
-                    <li class="media">
-                        <img class="media-object pull-left" src="{{{ asset('images/pan.jpg') }}}" alt="...">
-                        <div class="media-body">
-                            <h4 class="media-heading">Media heading</h4>
-                            <p class="list-group-item-text">The same is true of your blog homepage design: it could be the make-or-break reason why someone decides to dig deeper into your content or
-                                to leave the site</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-1">
+                <div class="card mb-3">
+                    <h4 class="card-header">ON/OFF Shore</h4>
+                    <img style="height: 200px; width: 100%; display: block;" src="https://www.tatvasoft.com/images/serviceprovider1.jpg" alt="Card image">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
             </div>
         </div>
+        <div class="col-md-1">
+        </div>
     </div>
-
 @stop
--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -1,4 +1,19 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script>
+    $(document).ready(function(){
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 2000, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
+</script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
